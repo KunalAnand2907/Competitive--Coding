10e6bda63df0412b4bef03036feb978d14e2a19b
KUNAL ANAND <46574881+KunalAnand2907@users.noreply.github.com>
2023-09-02 17:42:51 +0530

    Time: 117 ms (95.39%) | Memory: 31.5 MB (86.76%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.py
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.py
@@ -11,12 +11,12 @@ class Solution:
         while(l1!=l2 and l1!=None and l2!=None):
             l1 = l1.next
             l2 = l2.next
-            if l1==l2: # when common node exists, or when both equal len and no common node 
+            if l1==l2: # when common node exists, or when both equal len and no common node, or when not equal len & also no common ele then also will become null at same point
                 return l1
             if l1==None:
                 l1 = headB
             if l2 == None:
                 l2 = headA
-        return l2
+        return l1 # both are empty
         
         
