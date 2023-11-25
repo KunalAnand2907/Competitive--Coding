67ab9182aa9999dff768fdd2e5e8e2e495edc52b
KUNAL ANAND <46574881+KunalAnand2907@users.noreply.github.com>
2023-11-25 13:19:47 +0530

    Time: 167 ms (32.87%) | Memory: 15.4 MB (20.76%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.py
+++ 503-next-greater-element-ii/next-greater-element-ii.py
@@ -3,7 +3,7 @@ class Solution(object):
         # T(n):O(2n+2n), S(n)-O(n) - 2 pass Solution, Monotonic Decreasing Stack, while entering from last to first Mono Inc Stack
         st, ng = [], [-1]*len(A)
         n = len(A)
-        for i in range((2*n)-1,-1,-1):
+        for i in range(2*n-1,-1,-1):
             while(st!=[] and st[-1]<=A[i%n]):
                 st.pop()
             if (i<n and st!=[]):
