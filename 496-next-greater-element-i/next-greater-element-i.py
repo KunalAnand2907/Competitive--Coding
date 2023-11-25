959ea8acb5eb4667867ea412f8aae67b7aae875e
KUNAL ANAND <46574881+KunalAnand2907@users.noreply.github.com>
2023-11-25 13:14:56 +0530

    Time: 56 ms (50.74%) | Memory: 16.8 MB (14.14%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,15 +1,27 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        # Method 1: Brute Force
         # T(n): O(n.n-1) ~ O(n**2) - independant nested for loops , S(n): O(len(nums2))
         # Dict and 2 Nested Dep For loops
-        d={}
-        for i in range(len(nums2)):
-           next=-1
-           for j in range(i+1,len(nums2)):
-               if nums2[i]<nums2[j]:
-                    next = nums2[j]
-                    break
-           d[nums2[i]]=next
+        # d={}
+        # for i in range(len(nums2)):
+        #    next=-1
+        #    for j in range(i+1,len(nums2)):
+        #        if nums2[i]<nums2[j]:
+        #             next = nums2[j]
+        #             break
+        #    d[nums2[i]]=next
+        # return [d[k] for k in nums1]
+        # Method 2: Monotonic Dec Stack with 2 Virtual Pass
+        st = [] # Mon Dec Order
+        d = {k:-1 for i,k in enumerate(nums2)}
+        n = len(nums2)
+        for i in range(n-1,-1,-1):
+            while(st!=[] and st[-1]<=nums2[i]):
+                st.pop()
+            if st!=[]:
+                d[nums2[i]] = st[-1]
+            st.append(nums2[i])
         return [d[k] for k in nums1]
         
         
