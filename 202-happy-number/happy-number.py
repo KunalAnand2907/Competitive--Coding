0592dc1ec3986ac62cc6b5126f8f2adb45416e8c
KUNAL ANAND <46574881+KunalAnand2907@users.noreply.github.com>
2023-11-26 15:25:03 +0530

    Time: 43 ms (51.70%) | Memory: 16.2 MB (42.42%) - LeetSync

---
+++ 202-happy-number/happy-number.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        # T(n):O(), S(n): O(n)
+        visit = set()
+        while n not in visit:
+            visit.add(n)
+            n = self.sumOfsqaures(n)
+            if n==1:
+                return True
+        return False
+
+    def sumOfsqaures(self,n):
+        out = 0
+        while n!=0:
+            digit = n%10 # one's place digit
+            digit = digit**2
+            out +=digit
+            n = n//10
+        return out
+
+        
