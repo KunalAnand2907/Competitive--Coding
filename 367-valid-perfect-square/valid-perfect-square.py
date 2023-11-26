8e9c76e0bacd29a3ce164ab066b25bed931a458d
KUNAL ANAND <46574881+KunalAnand2907@users.noreply.github.com>
2023-11-26 11:20:21 +0530

    Time: 23 ms (8.84%) | Memory: 14.6 MB (5.23%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def isPerfectSquare(self, num):
+        # T(n) - O(root n)
+        for i in range(1,int(num**0.5)+1):
+            if i*i==num:
+                return True
+            elif i*i>num:
+                return False
+        # # T(n)-O(logn)- Binary Search
+        # l,r=1,num
+        # while(l<=r):
+        #     mid=(l+r)//2
+        #     if mid*mid>num:
+        #         r=mid-1 # remove the mid and upper half
+        #     elif mid*mid<num:
+        #         l=mid+1# remove the mid and lower half
+        #     else:
+        #         return True
+        # return False
