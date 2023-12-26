fd0eff0c02b8e53613c875ebaefb728857f04100
KUNAL ANAND <46574881+KunalAnand2907@users.noreply.github.com>
2023-12-27 03:43:57 +0530

    Time: 36 ms (70.61%) | Memory: 17.3 MB (6.61%) - LeetSync

---
+++ 50-powx-n/powx-n.py
@@ -0,0 +1,14 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        # Math+Recursion(Divide & conquer: 2^5 = 2^2.2^2.2): T(n):(log2n),S(n): O(n), Asked in Arcesium
+        def help(x,n):
+            # base cases
+            if x==0:
+                return 0
+            if n==0:
+                return 1
+            res = help(x*x,n//2)
+            # res = res*res
+            return x*res if n%2 else res
+        res = help(x,abs(n))
+        return res if n>=0 else 1/res
